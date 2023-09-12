9da76bf644ee32bab76b82e6731d212371615d00
leowu0511 <144814501+leowu0511@users.noreply.github.com>
2023-09-12 22:49:09 +0800

    社課1 cin,cout
    社課1 cin cout

---
+++ 社課1/社課1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+//include:引用入函式庫
+//<bits/stdc++.h>萬用標頭黨 
+using namespace std;
+//使用命名空間std,詳細建議google 
+
+
+//cin的程式 
+/*
+int main(){
+    int a=0;    //宣告一個變數
+    cin>>a;    //輸入
+    cout<<a;    //輸出
+}
+*/
+
+//cout的程式 
+/*
+int main()
+{
+    cout<<"早上好"
+}
+*/
