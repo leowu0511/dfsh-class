2d35f35481ebf9952cddb1f848d9a8f0147e4d2b
leowu0511 <144814501+leowu0511@users.noreply.github.com>
2023-10-25 23:27:46 +0800

    Update ifelse範例3.cpp

--- 社課2/第二次社課程式碼/ifelse範例3.cpp
+++ 社課2/第二次社課程式碼/ifelse範例3.cpp
@@ -4,12 +4,12 @@ int main(){
 	float bmi=0;
 	cin>>bmi;
 	if(bmi<18.5){
-		cout<<"¹L½G"; 
+		cout<<"éç˜¦"; 
 	}
 	else if(bmi>=18.5&&bmi<25){
-		cout<<"¥¿±`";
+		cout<<"é©ä¸­";
 	}
 	else if(bmi>=25){
-		cout<<"¹L­«";
+		cout<<"éé‡";
 	}
 }
